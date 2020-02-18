7a6166282c995fa41096c8b1971bf05f2d27ae4e
leksar <leksar2012@gmail.com>
2020-02-18 09:09:15 +0300

    messing with an axis #1

--- src/components/bar-chart/bar-chart.js
+++ src/components/bar-chart/bar-chart.js
@@ -4,48 +4,53 @@ import * as d3 from 'd3';
 const BarChart = ({ data = [], width = 400, height = 300 }) => {
 
     let canvas = React.createRef();
+    const margin = { top: 20, right: 30, bottom: 20, left: 30 }
+    width = width - margin.left - margin.right;
 
     useEffect(() => {
         const gapWidth = 1; //space between rects
         const barWidth = (width - gapWidth * data.length) / data.length
-        const svgCanvas = d3.select(canvas.current);
+        const svgCanvas = d3.select(canvas.current).style('transform', 'translate(' + margin.left + 'px,' + margin.top + 'px)');
 
         const y = d3.scaleLinear()
             .domain([0, d3.max(data)])
-            .range([0, height]);
+            .range([height, 0])
+            .nice();
 
         const rects = svgCanvas.selectAll('rect').data(data)
+
         rects.exit().remove()
         rects.enter().append('rect')
             .attr('width', barWidth)
-            .attr('height', d => y(d))
+            .attr('height', d => height - y(d))
             .attr('x', (_, idx) => idx * (barWidth + gapWidth))
-            .attr('y', d => height - y(d))
+            .attr('y', d => y(d))
 
         rects.attr('width', barWidth)
-            .attr('height', d => y(d))
+            .attr('height', d => height - y(d))
             .attr('x', (_, idx) => idx * (barWidth + gapWidth))
-            .attr('y', d => height - y(d))
+            .attr('y', d => y(d))
 
-        const texts = svgCanvas.selectAll('text').data(data)
-        texts.exit().remove()
-        texts.enter().append('text')
-            .attr('x', (_, idx) => idx * (barWidth + gapWidth))
-            .attr('y', d => height - y(d) + 20)
-            .text(d => d)
+        d3.selectAll('.x.axis').remove()
 
-        texts.attr('x', (_, idx) => idx * (barWidth + gapWidth))
-            .attr('y', d => height - y(d) + 20)
-            .text(d => d)
+        d3.select(canvas.current)
+            .append('g')
+            .attr("class", "x axis")
+            .call(d3.axisLeft(y));
     }, [data])
 
     return (
         <svg
             className='react-d3-templates-canvas'
-            width={width}
-            height={height}
-            ref={canvas}
-        />
+            width={width + margin.left + margin.right}
+            height={height + margin.top + margin.bottom}
+        >
+            <g
+                ref={canvas}
+                width={width - margin.left - margin.right}
+                height={height - margin.top - margin.bottom}
+            />
+        </svg>
     )
 }
 export default BarChart;
